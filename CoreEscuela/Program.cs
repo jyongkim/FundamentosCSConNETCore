7f3698fd70f96f3cb98bd7719c73bbb4aa1f320d
Jonathan Kim <jonathankim86@gmail.com>
2022-04-21 15:08:14 -0300

    platziHelloWorld and CoreEscuela

---
+++ CoreEscuela/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace CoreEscuela
+{
+    class Escuela
+    {
+        public string nombre;
+        public string direccion;
+        public int añoFundacion;
+        public string ceo;
+
+        public void Timbrar()
+        {
+            //ToDo
+            Console.Beep(987, 1000); //Si
+            Console.Beep(1174, 500); //Re'
+            Console.Beep(880, 1500); //La
+
+            Console.Beep(783, 250); //Sol
+            Console.Beep(880, 250); //La
+            Console.Beep(987, 1000); //Si
+
+            Console.Beep(1174, 500); //Re'
+            Console.Beep(880, 1500); //La
+
+            Console.Beep(987, 1000); //Si
+            Console.Beep(1174, 500); //Re'
+            Console.Beep(1760, 1000); //La'
+            Console.Beep(1567, 500); //Sol'
+            Console.Beep(1174, 1000); //Re'
+
+            Console.Beep(1046, 250); //Do
+            Console.Beep(987, 250); //Si
+            Console.Beep(880, 1000); //La
+
+            //Console.Beep(523, 500); 
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var miEscuela = new Escuela();
+            miEscuela.nombre = "Platzi Academy";
+            miEscuela.direccion = "Cr. 9 con calle 72";
+            miEscuela.añoFundacion = 2012;
+            Console.WriteLine("TIMBRE");
+            miEscuela.Timbrar();
+            //Console.WriteLine("Hello, world!");
+        }
+    }
+}
---
+++ platziHelloWorld/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace platziHelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("¡Hola, Platzi!");
+        }
+    }
+}
